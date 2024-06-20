e33df80336b9d4313bb402b05b53ad71220bc449
LuizEdu-AR <luiz.rodrigues67246@alunos.ufersa.edu.br>
2024-06-20 02:30:22 -0300

    update no css do header e z-index da logo do componente da home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,12 @@ import './styles.css'
 import LogoHeader from '../../assets/img/g-header.png'
 
 const Header = () => {
+
+  const handleNavigation = (e, sectionId) => {
+    e.preventDefault();
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='header-main-container'>
       <div className="logo-header-container">
@@ -11,10 +17,10 @@ const Header = () => {
       </div>
       <div className="options-header-container">
         <ul>
-          <li><a href="/#home-section">Home</a></li>
-          <li><a href="/#service-section">Serviços</a></li>
-          <li><a href="/#time-section">Equipe</a></li>
-          <li><a href="/#contact-section">Contato</a></li>
+          <li><a href="/#home-section" onClick={(e) => handleNavigation(e, 'home-section')}>Home</a></li>
+          <li><a href="/#service-section" onClick={(e) => handleNavigation(e, 'service-section')}>Serviços</a></li>
+          <li><a href="/#time-section" onClick={(e) => handleNavigation(e, 'time-section')}>Equipe</a></li>
+          <li><a href="/#contact-section" onClick={(e) => handleNavigation(e, 'contact-section')}>Contato</a></li>
         </ul>
       </div>
       <div className="git-header-container">
