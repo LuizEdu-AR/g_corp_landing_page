4b9e7f62e73116bb9ee4114b686fc670d1796388
LuizEdu-AR <luiz.rodrigues67246@alunos.ufersa.edu.br>
2024-06-20 02:57:34 -0300

    implementação de função de mudança da opacidade do header ao rolar a página

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,19 +3,37 @@ import './styles.css'
 import LogoHeader from '../../assets/img/g-header.png'
 
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
 
+  // função para controlar a navegação suave
   const handleNavigation = (e, sectionId) => {
     e.preventDefault();
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
   };
 
+  const [opacity, setOpacity] = useState(1);
+
+  // função para controlar a opacidade do header
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const newOpacity = Math.max(0.5, 1 - scrollTop / 500);
+      setOpacity(newOpacity);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='header-main-container'>
+    <div className='header-main-container' style={{ backgroundColor: `rgba(0, 18, 28, ${opacity})`}} >
       <div className="logo-header-container">
         <img src={LogoHeader} alt="Logo do Git" />
-        <Link to="/" style={{textDecoration:"none"}}><h3>- Corp <span className='blue-color'>Software</span> Solutions</h3></Link>
+        <Link to="/" style={{ textDecoration: "none" }}><h3>- Corp <span className='blue-color'>Software</span> Solutions</h3></Link>
       </div>
       <div className="options-header-container">
         <ul>
